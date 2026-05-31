03b83b64c046540ae2901f620fc61fc564827e9c
HLTienDat <hltiendat2002@gmail.com>
2026-06-01 05:41:05 +0700

    Initial Auth Library

---
+++ AuthService.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthLibrary;
+
+public class AuthService
+{
+    public string Login()
+    {
+        return "User logged in";
+    }
+
+    public string GenerateJWT()
+    {
+        return "JWT Token";
+    }
+}
